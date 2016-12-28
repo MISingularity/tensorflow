198d87d692ceca07aaef1f46af143d5a85fcfafb
plutoshe <plutoshe@gmail.com>
2016-12-28 14:27:41 +0800

    delete octbit and warpctc

--- tensorflow/contrib/naturali/__init__.py
+++ tensorflow/contrib/naturali/__init__.py
@@ -3,5 +3,3 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.contrib.naturali.python.ops.lookahead_ops import *
-from tensorflow.contrib.naturali.python.ops.octbit_ops import *
-from tensorflow.contrib.naturali.tools import *
